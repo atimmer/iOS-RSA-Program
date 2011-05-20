cd61eaa7f894c2bce99dbef78b316f4d571a323d
Marcel Boersma <marcelboersma@Marcel-Boersmas-MacBook-Pro.local>
2011-05-20 19:16:29 +0200

    adfsafsa

--- RSA.xcodeproj/KeyViewController.h
+++ RSA.xcodeproj/KeyViewController.h
@@ -2,14 +2,14 @@
 //  KeyViewController.h
 //  RSA
 //
-//  Created by Marcel Boersma(1) on 5/19/11.
+//  Created by Marcel Boersma on 5/20/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 
-@interface KeyViewController : UIViewController {
+@interface KeyViewController : UITableViewController {
     
 }
 
