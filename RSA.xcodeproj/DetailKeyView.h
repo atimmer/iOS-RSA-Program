18dae3e9c4370446168db21dc860bba0c854c65d
Marcel Boersma <marcelboersma@Marcel-Boersmas-MacBook-Pro.local>
2011-05-23 13:56:07 +0200

    Better table view, still without navigation controller

---
+++ RSA.xcodeproj/DetailKeyView.h
@@ -0,0 +1,16 @@
+//
+//  DetailKeyView.h
+//  RSA
+//
+//  Created by Marcel Boersma on 5/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DetailKeyView : UIViewController {
+    
+}
+
+@end
