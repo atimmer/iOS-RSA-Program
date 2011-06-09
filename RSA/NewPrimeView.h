07f2e3b7f60501ef0c15ce01efbab93bd909b596
Marcel Boersma <marcelboersma@Marcels-MacBook-Pro.local>
2011-06-09 20:55:57 +0200

    Added NewPrimeView for NewKeyView because it was different from PrimeView also fixed CoreData issues by adding NSFetchedResultsController and fixed some retain count issues. (To many releases Woops :$)

---
+++ RSA/NewPrimeView.h
@@ -0,0 +1,13 @@
+//
+//  NewPrimeView.h
+//  RSA
+//
+//  Created by Marcel Boersma on 6/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPrimeView : UIViewController
+
+@end
