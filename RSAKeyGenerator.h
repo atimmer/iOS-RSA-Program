e13497565489feb41fda6602abc4538a46f264e7
Marcel Boersma <marcelboersma@Marcels-MacBook-Pro.local>
2011-06-12 16:44:21 +0200

    Fixed the RSA Model, now the key calculation is correct. (It still takes the highest public key possible...) Encrypting en decrypting also added

---
+++ RSAKeyGenerator.h
@@ -0,0 +1,13 @@
+//
+//  RSAKeyGenerator.h
+//  RSA
+//
+//  Created by Marcel Boersma on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSAKeyGenerator : NSObject
+
+@end
---
+++ RSAKeyGenerator.m
@@ -0,0 +1,23 @@
+//
+//  RSAKeyGenerator.m
+//  RSA
+//
+//  Created by Marcel Boersma on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RSAKeyGenerator.h"
+
+@implementation RSAKeyGenerator
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
